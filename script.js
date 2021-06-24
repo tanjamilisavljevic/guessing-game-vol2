eeab01dea36024d3236c19693c243b8e98b05a55
Tanja <tanjamilisavljevic7@gmail.com>
2021-06-24 12:14:59 +0200

    Add label message

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-document.getElementById("guessButton").addEventListener("click", function () {
 
-    const peopleInOurGroup = 15;
+
+const peopleInOurGroup = 15;
+document.getElementById("guessLabel").innerHTML=`Guess a number from 1 to ${peopleInOurGroup}`;
+
+
+document.getElementById("guessButton").addEventListener("click", function () {
     let computerChoice = Math.floor(Math.random() * peopleInOurGroup + 1);
     console.log(computerChoice);
-
     const userChoice = (document.getElementById("theGuess")).value;
 
     if (computerChoice === parseInt(userChoice)) {
